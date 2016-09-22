63ec6fb33d923d3827c9102c71ac397969cb82e3
弈辰 <huijian.xhj@alibaba-inc.com>
2016-09-22 17:23:44 +0800

    update

--- app/src/main/java/com/xhj/huijian/library/utils/AppUtils.java
+++ app/src/main/java/com/xhj/huijian/library/utils/AppUtils.java
@@ -1,16 +1,33 @@
 package com.xhj.huijian.library.utils;
 
-import android.content.Intent;
-import android.os.Looper;
-import android.view.View;
-import android.widget.Toast;
+import android.app.ActivityManager;
+import android.content.Context;
+
+import java.util.List;
 
 /**
  * Created by Gavin on 16/9/21 下午5:32.
  * huijian.xhj@alibaba-inc.com
  * Project Name: Library.
+ * App 通用工具类
  */
 
 public class AppUtils {
-
+    /**
+     * 获取当前进程名称
+     * @param context
+     * @return
+     */
+    public static String getCurrentProcessName(Context context) {
+        String name = null;
+        final int pid = android.os.Process.myPid();
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> infos = activityManager.getRunningAppProcesses();
+        for (ActivityManager.RunningAppProcessInfo info : infos) {
+            if (pid == info.pid) {
+                name = info.processName;
+            }
+        }
+        return name;
+    }
 }
